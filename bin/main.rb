7e0e54ea3ebc308d1638b0b7b3948cbb71a13c0f
dasileker <dasileker@gmail.com>
2020-09-25 09:40:38 +0100

    added the players + board

--- bin/main.rb
+++ bin/main.rb
@@ -6,4 +6,17 @@ puts "WELCOME TO THIS TIC-TAC-TOE GAME"
 puts "Press 'ENTER' to begin"
 puts "Press 'ESC' to exit"
 
-ESC = gets.chomp
+gets.chomp
+
+#player1
+puts "\nPlayer 1:"
+player1 = gets.chomp
+
+puts "\nPlayer 2:"
+player2 = gets.chomp
+
+
+#numbers to use place in a square
+puts 'Pleas choose a number between 1-9'
+
+board = ['', '', '', '', '', '', '', '', '']
