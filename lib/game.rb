fd0472c7fb4db0f8da05a73b4223eb3d63f75977
sinework <sine.s.amare@gmail.com>
2020-09-25 16:12:29 +0300

    Update:initialize method of the game class is updated

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
+require_relative '../lib/player.rb'
 class Game
-    
-    def initialize
-        
-    end
+  WINNING_SET = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ].freeze
+
+  def initialize
+    @player1 = Player.new('', 'X')
+    @player1 = Player.new('', 'O')
+    @game_arr= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
 end
