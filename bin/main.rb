6923233afd6f9c30e0b41253438e4727e5ac73e8
dasileker <dasileker@gmail.com>
2020-09-25 12:05:50 +0100

    added the  moves for players

--- bin/main.rb
+++ bin/main.rb
@@ -11,4 +11,29 @@ class Tictactoe
     @name = []
   end
 
+  def welcome
+    puts `clear`
+    puts 'Welcome to Tic Tac Toe'
+    puts ' '
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '-----------'
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '-----------'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts ' '
+  end
+
+  def move(index, current_player)
+    @board[index] = @current_player
+    p board
+    puts "Now, #{current_player} move is displayed on the board"
+  end
+
+  def move_board(idx)
+    @board[idx] = 'X'
+  end
+
   
