f44cd1c23570d931eb883b0b1aae73ed112d821d
dasileker <dasileker@gmail.com>
2020-09-25 12:05:02 +0100

    refactored board and name players

--- bin/main.rb
+++ bin/main.rb
@@ -1,41 +1,14 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-puts "Hello, World!"
+require_relative '../lib/player'
 
-puts "WELCOME TO THIS TIC-TAC-TOE GAME"
-puts "Press 'ENTER' to begin"
-puts "Press 'ESC' to exit"
+class Tictactoe
+  attr_accessor :board, :name
 
-gets.chomp
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @name = []
+  end
 
-#player1
-puts "\nPlayer 1:"
-player1 = gets.chomp
-
-puts "\nPlayer 2:"
-player2 = gets.chomp
-
-
-#numbers to use place in a square
-puts 'Pleas choose a number between 1-9'
-
-board = ['', '', '', '', '', '', '', '', '']
-
-def display_board(board)
-  %(#{borad[0]} | #{borad[1]} | #{borad[2]}
-----------------
-#{borad[3]} | #{borad[4]} | #{borad[5]} 
-----------------
-#{borad[6]} | #{borad[7]} | #{borad[8]}
-  )
-end
-player1=gets.chomp
- puts "Amine chose #{player1}"
- puts 'Pleas choose a number between 1-9'
- player2=gets.chomp
- puts "Sine chose #{player2}"
-
- #until a winning combination is reached or the game is a draw the code will be excuted
-puts "This was a draw. Do you want to play again?"
-response = gets.chomp
-puts "The game has ended!" 
+  
