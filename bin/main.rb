6fc0ee4f1a309ac3bd0c0325d18cd9048af5f69d
sinework <sine.s.amare@gmail.com>
2020-09-27 20:44:13 +0300

    Update: updated the validate_so it flags when the user inputs anempty string or just a number as an input

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
+
 require_relative '../lib/player'
+
 require_relative '../lib/game'
 
 game = Game.new
@@ -8,34 +10,68 @@ board = game.game_arr
 
 def welcome_screen(game, board)
   name = ''
+
   puts 'Welcome to TIC-TAC-TOE'
+
+  puts "First player, What's your name?"
+
   loop do
-    puts "First player, What's your name?"
     name = gets.chomp
-    break if validate_name(name)
+
+    unless validate_name(name)
+
+      puts "Your name cannot be an empty line or a number \n \n"
+
+      puts "Please enter a valid name \n"
+
+    end
+
+    break if validate_name(name) == true
   end
+
   game.player1.name = name
+
   puts "Hi #{game.player1.name} , your key is #{game.player1.key} "
+
+  puts "Second player, What's your name?"
+
   loop do
-    puts "Second player, What's your name?"
     name = gets.chomp
-    break if validate_name(name)
+
+    unless validate_name(name)
+
+      puts "Your name cannot be an empty line or a number \n \n"
+
+      puts "Please enter a valid name \n"
+
+    end
+
+    break if validate_name(name) == true
   end
+
   game.player2.name = name
+
   puts "Hi #{game.player2.name} , your key is #{game.player2.key} "
+
   display_board(board)
 end
 
 # displaying the game board
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+
   puts '-----------'
+
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+
   puts '-----------'
+
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # Ask for input
+
 def ask_input(plyr)
   puts "#{plyr}, make your move!"
 end
@@ -45,35 +81,55 @@ def recieve_turns
 end
 
 # validate move
+
 def validate_move(mov, game_board)
   if !mov.between?(0, 8) || !game_board[mov].is_a?(Integer)
 
     false
+
   else
+
     Gem.win_platform? ? (system 'cls') : (system 'clear')
+
     true
+
   end
 end
 
 # invalid position
+
 def position_error
   puts 'please enter valid position'
 end
 
 # Validating the user name
+
 def validate_name(name)
-  return true unless ['', ' '].include?(name) && name[0].is_a?(String)
+  if name.scan(/\D/).empty?
+
+    false
+
+  else
+
+    true
+
+  end
 end
 
 # Annouce winner
+
 def announce_winner(name)
   Gem.win_platform? ? (system 'cls') : (system 'clear')
+
   puts "Congrats #{name}, you have won! "
 end
 
 def draw
   Gem.win_platform? ? (system 'cls') : (system 'clear')
+
   puts 'It is a draw!'
 end
+
 welcome_screen(game, board)
+
 game.start_game
